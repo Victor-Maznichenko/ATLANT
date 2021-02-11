d0789eb072ed3b97d640ee7868fcbf6ddaa33b4a
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2021-02-11 21:15:26 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function() {
+
+//page scrolling script
+  $('.fullpage').fullpage({
+   scrollHorizontally: true,
+   scrollOverflow: true,
+   sectionSelector: '.page',
+ });
+
+  //stylization of the select
+  $('.custom-select').styler();
+
+//page scrolling script
+// var fullpage = new Swiper('.fullpage', {
+//   slideClass: 'fullpage__slide',
+//   wrapperClass: 'fullpage__wrapper',
+//   direction: 'vertical',
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   mousewheel: true,
+// });
+
+
+// FadeIn logo on first page
+// fullpage.on('slideChangeTransitionEnd', function(){
+//   if ($('.first').hasClass('swiper-slide-active')) {
+//     $('.header__bottom-logo').addClass('hide');
+//   } else{
+//     $('.header__bottom-logo').removeClass('hide');
+//   }
+// });
+
+//page scrolling script
+var projectSlider = new Swiper('.projects__slider', {
+  wrapperClass: 'projects__slider-wrapper',
+  slideClass: 'projects__slide',
+  slidesPerView: 'auto',
+  spaceBetween: 128,
+  centeredSlides: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  observer: true,
+  loop: true,
+  pagination: {
+    el: '.projects__slider-pagination',
+    type: 'fraction',
+  },
+});
+
+
+});
