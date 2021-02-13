09f6bb0fd9994710996ed4aee0cf02e9b6b6ccad
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2021-02-13 10:54:35 +0300

    dfdf
    dfdf

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
+//Parallax images on the page work
+document.addEventListener('mousemove', parallax);
+function parallax(e){
+ this.querySelectorAll('.swiper-slide-active .work__slide-img').forEach(img => {
+   const speed = img.getAttribute('data-speed')
+   const x = (window.innerWidth - e.pageX*speed)/100
+   const y = (window.innerHeight - e.pageY*speed)/100
+   var myVar = setTimeout(myTimer, 200);
+   function myTimer() {
+    img.style.transform = 'translateX(' + x + '%) translateY(' + y + '%)';
+  }
+});
+}
 $(function() {
 
-//page scrolling script
-$('.fullpage').fullpage({
- scrollHorizontally: true,
- scrollOverflow: true,
- sectionSelector: '.page',
-});
+  //page scrolling script
+  $('.fullpage').fullpage({
+   scrollHorizontally: true,
+   scrollOverflow: true,
+   sectionSelector: '.page',
+ });
 
   //stylization of the select
   $('.custom-select').styler();
 
-//page scrolling script
-// var fullpage = new Swiper('.fullpage', {
-//   slideClass: 'fullpage__slide',
-//   wrapperClass: 'fullpage__wrapper',
-//   direction: 'vertical',
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   mousewheel: true,
-// });
-
-
-// FadeIn logo on first page
-// fullpage.on('slideChangeTransitionEnd', function(){
-//   if ($('.first').hasClass('swiper-slide-active')) {
-//     $('.header__bottom-logo').addClass('hide');
-//   } else{
-//     $('.header__bottom-logo').removeClass('hide');
-//   }
-// });
+  $('.burger-btn').on('click', function(){
+    $('.tab').toggleClass('active');
+  });
 
 //page scrolling script
 var projectSlider = new Swiper('.projects__slider', {
@@ -47,20 +44,81 @@ var projectSlider = new Swiper('.projects__slider', {
   },
 });
 
-
-$('.work__slide-images').magnificPopup({
-  delegate: 'a',
-  type: 'image',
-  tLoading: 'Loading images #%curr%...',
-  mainClass: 'mfp-img-mobile',
-  gallery: {
-    enabled: true,
-    navigateByImgClick: true,
+//Slider on the page work
+var workslider = new Swiper('.work__slider', {
+  wrapperClass: 'work__slider-wrapper',
+  slideClass: 'work__slide',
+  mousewheel: true,
+  normalizeSlideIndex: true,
+  spaceBetween: 200,
+  direction: 'vertical',
+  effect: 'fade',
+  speed: 0,
+  loop: true,
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  hashNavigation: true,
+  pagination: {
+    el: '.work__pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
   },
-  image: {
-    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-  }
+  on: {
+    init: function () {
+      $('.work__slider').removeClass('hide');
+    },
+  },
 });
 
+//Gallery for each slide on the page work
+$('.work__slide-images').each(function(){
+  $(this).magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    mainClass: 'mfp-with-zoom',
+    tLoading: 'Loading images #%curr%...',
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function(openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    },
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    }
+  });
+})
 
+//Filter styles on click
+var filter = $('.filters__item');
+filter.on('click', function(){
+  filter.removeClass('active');
+  $(this).addClass('active');
+});
+
+//Slider sponsors on the page about
+var sponsorsSlider = new Swiper('.about__sponsors-slider', {
+  wrapperClass: 'about__sponsors-wrapper',
+  slideClass: 'about__sponsors-slide',
+  mousewheel: true,
+  spaceBetween: 50,
+  speed: 500,
+  loop: true,
+  pagination: {
+    el: '.about__sponsors-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+});
 });
